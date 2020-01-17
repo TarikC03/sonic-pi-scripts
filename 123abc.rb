6d469ddc0c0f3761001ca8d1edc7bb6f637b413b
TarikC03 <45046515+TarikC03@users.noreply.github.com>
2020-01-17 15:31:15 -0800

    Add files via upload

---
+++ 123abc.rb
@@ -0,0 +1,31 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 170
+live_loop :looptyloop do
+  play:c4
+  sleep 0.4
+  play:e3
+  sleep 0.4
+  play:c2
+  sleep 0.4
+  play:e4
+  sleep 0.4
+  play:g4
+  sleep 0.4
+  play:b4
+  sleep 0.4
+  play:c5
+  sleep 0.4
+  play:c5
+  sleep 0.4
+  play:c5
+  sleep 0.5
+  play:c5
+  sleep 0.4
+  play:b4
+  sleep 0.4
+  play:g4
+  sleep 0.4
+  play:e4
+  sleep 0.4
+  play:c4
+end
